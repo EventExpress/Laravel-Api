5f3bccfdef2f55f1ea58841c591a7ff2101401b8
Guilherme <guilher33@gmail.com>
2024-10-05 20:16:09 -0300

    fix:correcting the order of the tables and creating another category

---
+++ database/migrations/2024_10_05_225948_categoria_servicos.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('anuncio_categoria', function (Blueprint $table) {
+            $table->unsignedBigInteger('servicos_id');
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('servico_id')->references('id')->on('servicos')->onDelete('cascade');
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
