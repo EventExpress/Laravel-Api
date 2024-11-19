74308b475a44192b97b241d631fb82edd3c7018e
Guilherme <guilher33@gmail.com>
2024-11-18 22:42:48 -0300

    fix: ajuste servicos por cidade

--- app/Http/Controllers/AnuncioController.php
+++ app/Http/Controllers/AnuncioController.php
@@ -413,12 +413,16 @@ class AnuncioController extends Controller
     public function getServicosPorCidade($anuncio_id)
     {
         $anuncio = Anuncio::find($anuncio_id);
+
         $cidadeAnuncio = $anuncio->endereco->cidade;
 
-        $servicos = Servico::where('cidade', $cidadeAnuncio)->get();
+        $servicos = Servico::where('cidade', $cidadeAnuncio)
+            ->with('scategorias')
+            ->get();
 
         return response()->json($servicos);
     }
+
 
     public function getAvaliacoesAnuncio($anuncioId)
     {
